a3ff633188664290f8b255ea9148e77a6ceb7264
supervoid0 <sababnazmus@gmail.com>
2024-03-29 09:39:11 +0900

    add schema

---
+++ schemas/init.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+let conn = null;
+
+module.exports = async mongoUri => {
+  try {
+    if (conn == null) {
+      conn = mongoose
+        .connect(mongoUri, {
+          keepAlive: true,
+          autoIndex: true
+        })
+        .then(() => mongoose);
+
+      mongoose.connection.on('error', () => {
+        throw new Error(`unable to connect to database: ${mongoUri}`);
+      });
+
+      // `await`ing connection after assigning to the `conn` variable
+      // to avoid multiple function calls creating new connections
+      await conn;
+    }
+    return conn;
+  } catch (error) {
+    console.log(error);
+  }
+};
